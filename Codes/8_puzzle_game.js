57cdcc8f96733c22d4468c19c53d8a0f71573cc2
SifatSikder <bsse1221@iit.du.ac.bd>
2023-08-16 16:15:13 +0600

    Added adversial_search.pptx slide

--- Codes/8_puzzle_game.js
+++ Codes/8_puzzle_game.js
@@ -244,11 +244,17 @@ function game(initialState, finalState) {
 //         [4, 5, 6],
 //         [0, 7, 8]
 //     ]
+// const initialState =
+//     [
+//         [5, 2, 8],
+//         [0, 1, 7],
+//         [4, 3, 6]
+//     ]
 const initialState =
     [
-        [5, 2, 8],
-        [0, 1, 7],
-        [4, 3, 6]
+        [7, 2, 4],
+        [5, 0, 6],
+        [8, 3, 1]
     ]
 // var initialState =
 //     [
@@ -258,9 +264,9 @@ const initialState =
 //     ]
 const finalState =
     [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 0]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
     ]
 
 
